4fa788669782fae2f4ec28e00925eae1929ff7bd
Carrilho <rafael.carrilho@injunior.com.br>
2021-08-23 19:29:31 -0300

    add id on school year and getSchoolYear on subjects

--- models/classes/Subject.java
+++ models/classes/Subject.java
@@ -4,17 +4,18 @@ import models.interfaces.QueryInterface;
 
 public class Subject {
     private String name, ch, knowledge_area;
-    private int id, department_id;
+    private int id, department_id, school_year_id;
     private static String last_subject_id = QueryInterface.last("subjects").split(" \\| ")[0];
-    private static int quant_subjects = last_subject_id == null ? 0 : Integer.parseInt(last_subject_id);
+    private static int next_subject_id = last_subject_id == null ? 1 : Integer.parseInt(last_subject_id + 1);
 
-    public Subject (String name, String knowledge_area, String ch, int department_id){
+    public Subject (String name, String knowledge_area, String ch, int department_id, int school_year_id){
         this.name = name;
         this.knowledge_area = knowledge_area;
         this.ch = ch;
         this.department_id = department_id;
-        quant_subjects++;
-        this.id = quant_subjects;
+        this.school_year_id = school_year_id;
+        this.id = next_subject_id;
+        next_subject_id++;
     }
 
     public Subject (String subject_stringified){
@@ -24,10 +25,11 @@ public class Subject {
         this.knowledge_area = parameters[2];
         this.ch = parameters[3];
         this.department_id = Integer.parseInt(parameters[4]);
+        this.school_year_id = Integer.parseInt(parameters[5]);
     }
     
     public String stringify(){
-        return this.id + " | " + this.name + " | " + this.knowledge_area + " | " + this.ch + " | " + this.department_id;
+        return this.id + " | " + this.name + " | " + this.knowledge_area + " | " + this.ch + " | " + this.department_id + " | " + this.school_year_id;
     }
 
     public int getId() {
@@ -36,6 +38,10 @@ public class Subject {
     public Department getDepartment() {
         Department department = new Department(QueryInterface.find("departments", department_id));
         return department;
+    }
+    public SchoolYear getSchoolYear() {
+        SchoolYear school_year = new SchoolYear(QueryInterface.find("schoolyears", school_year_id));
+        return school_year;
     }
     public String getCh() {
         return ch;
