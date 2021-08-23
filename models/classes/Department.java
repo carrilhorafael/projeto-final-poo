97076e89e6a3817734495a77d7d11ec225cced07
Carrilho <rafael.carrilho@injunior.com.br>
2021-08-23 18:55:53 -0300

    change department ids for integer with parseInt and toString methods

--- models/classes/Department.java
+++ models/classes/Department.java
@@ -3,28 +3,29 @@ package models.classes;
 import db.QueryInterface;
 
 public class Department {
-    private String id, name, knowledge_area, campus, code, department_coordinator_id;
+    private String name, knowledge_area, campus, code;
+    private int id, department_coordinator_id;
     private static String last_department_id = QueryInterface.last("departments").split(" \\| ")[0];
     private static int quant_departments = last_department_id == null ? 0 : Integer.parseInt(last_department_id);
 
-    public Department (String name, String knowledge_area, String campus, String code, String department_coordinator_id){
+    public Department (String name, String knowledge_area, String campus, String code, int department_coordinator_id){
         this.name = name;
         this.knowledge_area = knowledge_area;
         this.campus = campus;
         this.code = code;
         this.department_coordinator_id = department_coordinator_id;
         quant_departments++;
-        this.id = Integer.toString(quant_departments);
+        this.id = quant_departments;
     }
 
     public Department (String department_stringified){
         String[] parameters = department_stringified.split(" \\| "); 
-        this.id = parameters[0];
+        this.id = Integer.parseInt(parameters[0]);
         this.name = parameters[1];
         this.knowledge_area = parameters[2];
         this.campus = parameters[3];
         this.code = parameters[4];
-        this.department_coordinator_id = parameters[5];
+        this.department_coordinator_id = Integer.parseInt(parameters[5]);
     }
     
     public String stringify(){
@@ -38,9 +39,9 @@ public class Department {
         return code;
     }
     public String getDepartmentCoordinator() {
-        return QueryInterface.find("users", department_coordinator_id);
+        return QueryInterface.find("users", Integer.toString(department_coordinator_id));
     }
-    public String getId() {
+    public int getId() {
         return id;
     }
     public String getKnowledgeArea() {
