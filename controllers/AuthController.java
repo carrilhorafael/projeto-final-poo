13c23a4a9c413b389df9362260d07a5812051663
Carrilho <rafael.carrilho@injunior.com.br>
2021-08-24 11:32:01 -0300

    change register action from authcontroller to usercontroller

--- controllers/AuthController.java
+++ controllers/AuthController.java
@@ -5,14 +5,6 @@ import models.classes.Manager;
 import models.interfaces.QueryInterface;
 
 public class AuthController {
-    public static boolean register(String[] parameters, int kind){
-        if (kind == 1){
-            Manager manager = new Manager(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parameters[7]);
-            
-            return QueryInterface.save("users", manager.stringify());
-        }
-        return false;
-    }
     
     public static User login(String email, String password){
         String[] user_stringified_params = QueryInterface.find_by("users", "email", email).split(" \\| ");
