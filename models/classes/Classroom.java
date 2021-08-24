fbce43234391eb823141335531fb0c7fee47d9be
Carrilho <rafael.carrilho@injunior.com.br>
2021-08-23 22:23:31 -0300

    fix classroom.java

--- models/classes/Classroom.java
+++ models/classes/Classroom.java
@@ -6,7 +6,7 @@ public class Classroom {
     private String code, room;
     private int id, teacher_id, subject_id;
     private static String last_classroom = QueryInterface.last("classrooms");
-    private static int next_classroom_id = last_classroom == null ? 1 : Integer.parseInt(last_classroom_id.split(" \\| ")[0]) + 1;
+    private static int next_classroom_id = last_classroom == null ? 1 : Integer.parseInt(last_classroom.split(" \\| ")[0]) + 1;
     
     public Classroom (String code, String room, int teacher_id, int subject_id){
         this.code = code;
